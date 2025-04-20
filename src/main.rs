7f434eb0916feb8059a010c1e1b3867636ef9a0c
Maeevick <maeevick@gmail.com>
2025-04-20 16:03:09 +0200

    feat: 'hello the greelins' the first query

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, state::commands};
 
 fn main() {
     App::new()
-        .add_systems(Update, hello_world_system)
+        .add_systems(Startup, add_character)
+        .add_systems(Startup, hello_world_system)
+        .add_systems(Update, greet_characters)
         .run();
 }
 
 fn hello_world_system() {
-    println!("hello, bevy engine!");
+    println!("\n----------------------");
+    println!("  HELLO, BEVY ENGINE!");
+    println!("----------------------\n");
  }
+
+ #[derive(Component)]
+ struct Character;
+
+ #[derive(Component)]
+struct Name(String);
+
+
+ fn add_character(mut commands: Commands) {
+    commands.spawn((Character, Name("Zogg Goldclaw".to_string())));
+    commands.spawn((Character, Name("Nibbles The Backscratcher".to_string())));
+    commands.spawn((Character, Name("Wizzle \"Professor\" Fumblepotion".to_string())));
+    commands.spawn((Character, Name("Grimtwig \"Titanarm\" Wobblefist".to_string())));
+    commands.spawn((Character, Name("Skritch Rustyknuckle".to_string())));
+ }
+
+ fn greet_characters(query: Query<&Name, With<Character>>) {
+    println!("--- The Greenlins! ---");
+    for name in &query {
+        println!("\thi {}!", name.0);
+    }
+}
